bdd19a51d6b83ae8c29c38f2c3f2522d59cbefcd
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-01 17:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let timer = document.getElementById("timer");
+
+function dateTime(){
+	let currentDate = new Date();
+
+	let month = currentDate.getMonth()+1;
+	let date = currentDate.getDate();
+	let year = currentDate.getFullYear();
+
+	let formattedDate = month + "/" + date + "/" + year;
+
+	let hours = currentDate.getHours();
+	let minutes = currentDate.getMinutes();
+	let seconds = currentDate.getSeconds();
+
+	let period = hours >= 12 ? PM : AM;
+
+	let formattedTime = hours + ":" + minutes + ":" + seconds + period;
+	timer.append(formattedDate + ", ");
+	timer.append(formattedTime);
+}
+dateTime();
